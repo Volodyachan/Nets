0c516b0356903d20cb8ba59bf191b7b3108b6c1c
Vladimir Giro <vogiro@edu.hse.ru>
2024-06-15 18:17:37 +0300

    fix

--- lab2/mtu_solution.py
+++ lab2/mtu_solution.py
@@ -22,7 +22,6 @@ def ping_host(target, size):
     
     try:
         response = subprocess.run(['ping', target, count_flag, '1'] + no_fragment_flag + [size_flag, str(size)], capture_output=True)
-        print(response.stderr, response.stdout)
     except Exception:
         return 0
 
@@ -39,14 +38,14 @@ def mtu_detection():
     args = parser.parse_args()
 
     if not args.host:
-        print('Ошибка: требуется указать хост.')
+        print('Ошибка: хост не указан')
         return 1
 
     target_host = args.host
     full_mode = args.full_mode
 
     if not validate_host(target_host):
-        print(f'Имя хоста {target_host} не может быть разрешено или некорректно')
+        print(f'Имя хоста {target_host} некорректно или не может быть разрешено')
         return 1
 
     try:
@@ -64,18 +63,18 @@ def mtu_detection():
         successful_ping = ping_host(target_host, midpoint)
 
         if successful_ping == 0:
-            print('Возникло неожиданное исключение при определении MTU')
+            print('Исключение MTU')
             return 1
         elif successful_ping == 1:
             low = midpoint
             if full_mode:
-                print(f'Пинг хоста {target_host} с размером данных {midpoint}, код возврата: 0')
+                print(f'Пинг {target_host} с размером {midpoint}, код: 0')
         else:
             high = midpoint
             if full_mode:
-                print(f'Пинг хоста {target_host} с размером данных {midpoint}, код возврата: 1')
+                print(f'Пинг {target_host} с размером {midpoint}, код: 1')
 
-    print(f'Определенный MTU для хоста {target_host} = {low} байт, размер пакета с заголовками = {low + 28} байт')
+    print(f'MTU для хоста {target_host} = {low} байт, размер полного пакета = {low + 28} байт')
 
 
 if __name__ == '__main__':
